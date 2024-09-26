67c84c59240c5ba46624a4a114671f7f03104eb8
eugenioidaea <eugenio.pescimoro@idaea.csic.es>
2024-09-26 14:42:36 +0200

    End of 1D particle tracking test

--- Codes/Python/diffAndReact.py
+++ Codes/Python/diffAndReact.py
@@ -1,22 +1,3 @@
-""" import numpy as np
-import matplotlib.pyplot as plt
-
-# Define the range for x and y
-x = np.linspace(0, 1, 10)
-y = np.linspace(0, 1, 10)
-
-# Generate the meshgrid
-X, Y = np.meshgrid(x, y)
-
-# Plot the 2D mesh
-plt.figure(figsize=(5, 5))
-plt.plot(X, Y, marker='.', color='k', linestyle='none')  # Mesh points
-plt.grid(True)
-plt.title("2D Grid Mesh")
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.show() """
-
 import numpy as np
 import matplotlib.pyplot as plt
 import math
