3eea9b656adc505facd35e5b4e573b7b2c34bb10
eugenioidaea <eugenio.pescimoro@idaea.csic.es>
2024-10-10 19:12:06 +0200

    Comments added

--- Codes/Python/diffAndReact2Dmatrix_vectorised.py
+++ Codes/Python/diffAndReact2Dmatrix_vectorised.py
@@ -32,6 +32,7 @@ reflectedOutward = 30 # Percentage of impacts from the porous matrix reflected a
 animatedParticle = 0 # Index of the particle whose trajectory will be animated
 fTstp = 0 # First time step to be recorded in the video
 lTstp = 90 # Final time step to appear in the video
+bins = 100 # Number of bins for the logarithmic plot
 
 noiseMatrix = np.sqrt(2*Dm*dt)  # Strength of the noise term for particle in the porous matrix
 noiseFracture = np.sqrt(2*Df*dt)  # Strength of the noise term for particle in the fracture
@@ -51,42 +52,46 @@ outsideFractureDown = False
 
 t = 0
 pdf_part = []
-# Initialize arrays to store position data
-x = np.zeros(num_particles)
-y = np.linspace(lby+init_shift, uby-init_shift, num_particles)
-xPath = [[] for _ in range(num_particles)]
-yPath = [[] for _ in range(num_particles)]
+x = np.zeros(num_particles) # Horizontal initial positions
+y = np.linspace(lby+init_shift, uby-init_shift, num_particles) # Vertical initial positions
+xPath = [[] for _ in range(num_particles)] # List for storing the trajectories
+yPath = [[] for _ in range(num_particles)] # List for storing the trajectories
 
-start_time = time.time()
+start_time = time.time() # Start timing the while loop
+
+# Time loop ###########################################################################
 
 while t<num_steps*dt:
 
     t = t + dt
 
-    isIn = x<rbx
+    isIn = x<rbx # Get the positions of the particles that are still in the domain
 
+    # Update ALL the particles' position for time step t
     x[isIn] = x[isIn] + np.sqrt(2*Dm*dt)*np.random.normal(meanEta, stdEta, np.sum(isIn))
     y[isIn] = y[isIn] + np.sqrt(2*Dm*dt)*np.random.normal(meanEta, stdEta, np.sum(isIn))
 
+    # Elastic reflection for the particles which hit the fracture's walls
     x = np.where(x<lbx, -x+2*lbx, x)
     y = np.where(y>uby, -y+2*uby, y)
     y = np.where(y<lby, -y+2*lby, y)
 
+    # Slightly less efficient implementation of the reflection boundary condition
     '''x[x<lbx] = -x[x<lbx] + 2*lbx
     y[y<lby] = -y[y<lby] + 2*lby
     y[y>uby] = -y[y>uby] + 2*lby'''
 
+    # Store the positions of each particle for all the time steps 
     if recordTrajectories:
         [xPath[p].append(float(x_val)) for p, x_val in enumerate(x) if x[p]<rbx]
         [yPath[p].append(float(y_val)) for p, y_val in enumerate(y) if x[p]<rbx]
     
-    pdf_part.append(sum(x[isIn]>rbx))
+    pdf_part.append(sum(x[isIn]>rbx)) # Count the particle which exit the right boundary at each time step
 
-end_time = time.time()
+end_time = time.time() # Stop timing the while loop
 execution_time = end_time - start_time
 
-Time = np.linspace(dt, t, len(pdf_part))
-
+# Plot section #########################################################################
 if plotCharts and recordTrajectories:
     plt.figure(figsize=(8, 8))
     for i in range(num_particles):
@@ -100,7 +105,7 @@ if plotCharts and recordTrajectories:
     plt.grid(True)
     plt.show()
 
-
+Time = np.linspace(dt, t, len(pdf_part))
 pdf_part = np.asarray(pdf_part)
 plt.figure(figsize=(8, 8))
 plt.plot(Time, pdf_part/num_particles)
@@ -109,13 +114,14 @@ plt.plot(Time, np.cumsum(pdf_part)/num_particles)
 
 i = 0
 particlesTstep=np.zeros(num_particles)
+# Retrieve the number of steps for each particle from the pdf of the breakthrough curve
 for index, value in enumerate(pdf_part):
     particlesTstep[i:i+value] = index
     i = i+value
-bins = 100
-timeLinSpaced = np.linspace(dt, t, bins)
-timeLogSpaced = np.logspace(np.log10(dt), np.log10(t), bins)
-counts, bin_edges = np.histogram(particlesTstep, timeLinSpaced)
+# Logarithmic plot
+timeLinSpaced = np.linspace(dt, t, bins) # Linearly spaced bins
+timeLogSpaced = np.logspace(np.log10(dt), np.log10(t), bins) # Logarithmic spaced bins
+counts, bin_edges = np.histogram(particlesTstep, timeLogSpaced)
 plt.figure(figsize=(8, 8))
 plt.plot(bin_edges[1:], counts)
 
