4d9b726ebc037ff9e88aa49436641a29ad2730a8
eugenioidaea <eugenio.pescimoro@idaea.csic.es>
2024-10-18 13:16:37 +0200

    Verification of the code started

--- Codes/Python/diffAndReact2Dmatrix_vectorised.py
+++ Codes/Python/diffAndReact2Dmatrix_vectorised.py
@@ -7,6 +7,7 @@ import time
 plotCharts = True # It controls graphical features (disable when run on HPC)
 recordVideo = False # It slows down the script
 recordTrajectories = False # It uses up memory
+yesRbx = True # It controls the right boundary condition
 
 if plotCharts:
     import matplotlib.pyplot as plt
@@ -19,7 +20,7 @@ Df = 1  # Diffusion for particles moving in the fracture
 dt = 1 # Time step
 meanEta = 0 # Spatial jump distribution paramenter
 stdEta = 1 # Spatial jump distribution paramenter
-num_particles = int(1e4) # Number of particles in the simulation
+num_particles = int(1e3) # Number of particles in the simulation
 uby = 1 # Vertical Upper Boundary
 lby = -1 # Vertical Lower Boundary
 lbx = 0 # Horizontal Left Boundary
@@ -53,8 +54,9 @@ timeLogSpaced = np.logspace(np.log10(dt), np.log10(dt*num_steps), bins) # Logari
 
 # Functions ##########################################################################
 
-def apply_reflection(x, y, crossInToOutAbove, crossInToOutBelow,  crossOutToInAbove, crossOutToInBelow, uby, lby):
-    x = np.where(x<lbx, -x+2*lbx, x)
+def apply_reflection(x, y, crossInToOutAbove, crossInToOutBelow,  crossOutToInAbove, crossOutToInBelow, uby, lby, yesRbx):
+    if yesRbx:
+        x = np.where(x<lbx, -x+2*lbx, x)
     y = np.where(crossInToOutAbove, -y+2*uby, y)
     y = np.where(crossInToOutBelow, -y+2*lby, y)
     y = np.where(crossOutToInAbove, -y+2*uby, y)
@@ -104,7 +106,7 @@ while (cdf<stopBTC/100*num_particles) & (t<num_steps*dt):
     crossOutToInBelow = crossInBelow & crossOutToIn # Below lower boundary y
     
     # Update the reflected particles' positions according to an elastic reflection dynamic
-    x, y = apply_reflection(x, y, crossInToOutAbove, crossInToOutBelow,  crossOutToInAbove, crossOutToInBelow, uby, lby)
+    x, y = apply_reflection(x, y, crossInToOutAbove, crossInToOutBelow,  crossOutToInAbove, crossOutToInBelow, uby, lby, yesRbx)
 
     inside = (y<uby) & (y>lby) # Particles inside the fracture
     outsideAbove = y>uby # Particles in the porous matrix above the fracture
@@ -135,7 +137,8 @@ if plotCharts and recordTrajectories:
         plt.plot(xPath[i], yPath[i], lw=0.5)
     plt.axhline(y=uby, color='r', linestyle='--', linewidth=2)
     plt.axhline(y=lby, color='r', linestyle='--', linewidth=2)
-    plt.axvline(x=lbx, color='black', linestyle='-', linewidth=2)
+    if yesRbx:
+        plt.axvline(x=lbx, color='black', linestyle='-', linewidth=2)
     plt.title("2D Diffusion Process (Langevin Equation)")
     plt.xlabel("X Position")
     plt.ylabel("Y Position")
