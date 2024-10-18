cdf99efa766a7a72d78ca992b4420d8c8efd3917
eugenioidaea <eugenio.pescimoro@idaea.csic.es>
2024-10-18 18:11:39 +0200

    Nothing wokrs

--- Codes/Python/diffAndReact2Dmatrix_vectorised.py
+++ Codes/Python/diffAndReact2Dmatrix_vectorised.py
@@ -6,21 +6,21 @@ import time
 # Features ###################################################################
 plotCharts = True # It controls graphical features (disable when run on HPC)
 recordVideo = False # It slows down the script
-recordTrajectories = False # It uses up memory
-yesRbx = True # It controls the right boundary condition
+recordTrajectories = True # It uses up memory
+rbxOn = True # It controls the right boundary condition
 
 if plotCharts:
     import matplotlib.pyplot as plt
     from matplotlib.animation import FuncAnimation
 
 # Parameters #################################################################
-num_steps = int(1e4) # Number of steps
+num_steps = int(1e3) # Number of steps
 Dm = 1  # Diffusion for particles moving in the porous matrix
 Df = 1  # Diffusion for particles moving in the fracture
 dt = 1 # Time step
 meanEta = 0 # Spatial jump distribution paramenter
 stdEta = 1 # Spatial jump distribution paramenter
-num_particles = int(1e3) # Number of particles in the simulation
+num_particles = int(1e2) # Number of particles in the simulation
 uby = 1 # Vertical Upper Boundary
 lby = -1 # Vertical Lower Boundary
 lbx = 0 # Horizontal Left Boundary
@@ -31,7 +31,9 @@ reflectedOutward = 20 # Percentage of impacts from the porous matrix reflected a
 animatedParticle = 0 # Index of the particle whose trajectory will be animated
 fTstp = 0 # First time step to be recorded in the video
 lTstp = 90 # Final time step to appear in the video
-bins = 50 # Number of bins for the logarithmic plot
+binsTime = 50 # Number of temporal bins for the logarithmic plot
+binsSpace = 80 # Number of spatial bins for the concentration profile
+recordSpatialConc = int(1e2) # Concentration profile recorded time
 stopBTC = 100 # % of particles that need to pass the control plane before the simulation is ended
 
 # Initialisation ####################################################################
@@ -49,25 +51,26 @@ outsideAbove = [False for _ in range(num_particles)]
 outsideBelow = [False for _ in range(num_particles)]
 particleRT = np.zeros(num_particles) # Array which stores each particles' number of steps
 Time = np.linspace(dt, num_steps*dt, num_steps) # Array that stores time steps
-timeLinSpaced = np.linspace(dt, dt*num_steps, bins) # Linearly spaced bins
-timeLogSpaced = np.logspace(np.log10(dt), np.log10(dt*num_steps), bins) # Logarithmic spaced bins
+timeLinSpaced = np.linspace(dt, dt*num_steps, binsTime) # Linearly spaced bins
+timeLogSpaced = np.logspace(np.log10(dt), np.log10(dt*num_steps), binsTime) # Logarithmic spaced bins
+xBins = np.linspace(0, rbx, binsSpace) # Linearly spaced bins
 
 # Functions ##########################################################################
-
-def apply_reflection(x, y, crossInToOutAbove, crossInToOutBelow,  crossOutToInAbove, crossOutToInBelow, uby, lby, yesRbx):
-    if yesRbx:
-        x = np.where(x<lbx, -x+2*lbx, x)
-    y = np.where(crossInToOutAbove, -y+2*uby, y)
-    y = np.where(crossInToOutBelow, -y+2*lby, y)
-    y = np.where(crossOutToInAbove, -y+2*uby, y)
-    y = np.where(crossOutToInBelow, -y+2*lby, y)
-    return x, y
 
 def update_positions(x, y, fracture, matrix, Df, Dm, dt, meanEta, stdEta):
     x[fracture] += np.sqrt(2*Df*dt)*np.random.normal(meanEta, stdEta, np.sum(fracture))
     y[fracture] += np.sqrt(2*Df*dt)*np.random.normal(meanEta, stdEta, np.sum(fracture))
     x[matrix] += np.sqrt(2*Dm*dt)*np.random.normal(meanEta, stdEta, np.sum(matrix))
     y[matrix] += np.sqrt(2*Dm*dt)*np.random.normal(meanEta, stdEta, np.sum(matrix))
+    return x, y
+
+def apply_reflection(x, y, crossInToOutAbove, crossInToOutBelow,  crossOutToInAbove, crossOutToInBelow, uby, lby, rbxOn):
+    if rbxOn:
+        x = np.where(x<lbx, -x+2*lbx, x)
+    y = np.where(crossInToOutAbove, y, -y+2*uby)
+    y = np.where(crossInToOutBelow, y, -y+2*lby)
+    y = np.where(crossOutToInAbove, y, -y+2*uby)
+    y = np.where(crossOutToInBelow, y, -y+2*lby)
     return x, y
 
 # Time loop ###########################################################################
@@ -81,7 +84,10 @@ while (cdf<stopBTC/100*num_particles) & (t<num_steps*dt):
         xPath[:, t] = x  # Store x positions for the current time step
         yPath[:, t] = y  # Store y positions for the current time step        
 
-    isIn = x<rbx # Get the positions of the particles that are in the domain (wheter inside or outside the fracture)
+    if rbxOn:
+        isIn = x<rbx # Get the positions of the particles that are in the domain (wheter inside or outside the fracture)
+    else:
+        isIn = np.ones(num_particles, dtype=bool)
     fracture = isIn & inside # Particles in the fracture
     outside = np.array(outsideAbove) | np.array(outsideBelow) # Particles outside the fracture
     matrix = isIn & outside # Particles in the domain and outside the fracture
@@ -90,29 +96,37 @@ while (cdf<stopBTC/100*num_particles) & (t<num_steps*dt):
     x, y = update_positions(x, y, fracture, matrix, Df, Dm, dt, meanEta, stdEta)
 
     # Particles which in principles would cross the fractures' walls
-    crossOutAbove = inside & (y>uby)
-    crossOutBelow = inside & (y<lby)
+    crossOutAbove = fracture & (y>uby)
+    crossOutBelow = fracture & (y<lby)
     crossInAbove = outsideAbove  & (y > lby) & (y < uby)
     crossInBelow = outsideBelow & (y>lby) & (y<uby)
 
     # Successfull crossing based on uniform probability distribution
-    crossInToOut = np.random.rand(num_particles) < reflectedInward/100
-    crossOutToIn = np.random.rand(num_particles) > reflectedOutward/100
-
+    # crossInToOut = np.random.rand(num_particles) > reflectedInward/100
+    # crossOutToIn = np.random.rand(num_particles) > reflectedOutward/100
+    crossOutAbove[np.where(crossOutAbove)[0]] = (np.random.rand(np.sum(crossOutAbove)) > reflectedInward/100) & (crossOutAbove[np.where(crossOutAbove)[0]])
+    crossOutBelow[np.where(crossOutBelow)[0]] = (np.random.rand(np.sum(crossOutBelow)) > reflectedInward/100) & (crossOutBelow[np.where(crossOutBelow)[0]])
+    crossInAbove[np.where(crossInAbove)[0]] = (np.random.rand(np.sum(crossInAbove)) > reflectedOutward/100) & (crossInAbove[np.where(crossInAbove)[0]])
+    crossOutBelow[np.where(crossInAbove)[0]] = (np.random.rand(np.sum(crossInBelow)) > reflectedOutward/100) & (crossInBelow[np.where(crossInBelow)[0]])
+    
     # Find the indeces of those particles that did not successfully corss the fracture
-    crossInToOutAbove = crossOutAbove & crossInToOut # Above upper boundary y
-    crossInToOutBelow = crossOutBelow & crossInToOut # Below lower boundary y
-    crossOutToInAbove = crossInAbove & crossOutToIn # Above upper boundary y
-    crossOutToInBelow = crossInBelow & crossOutToIn # Below lower boundary y
+    # crossInToOutAbove = crossOutAbove & crossOutAbove[crossOutAbove] & crossOut # Above upper boundary y
+    # crossInToOutBelow = crossOutBelow & crossInToOut # Below lower boundary y
+    # crossOutToInAbove = crossInAbove & crossOutToIn # Above upper boundary y
+    # crossOutToInBelow = crossInBelow & crossOutToIn # Below lower boundary y
     
     # Update the reflected particles' positions according to an elastic reflection dynamic
-    x, y = apply_reflection(x, y, crossInToOutAbove, crossInToOutBelow,  crossOutToInAbove, crossOutToInBelow, uby, lby, yesRbx)
+    # x, y = apply_reflection(x, y, crossInToOutAbove, crossInToOutBelow,  crossOutToInAbove, crossOutToInBelow, uby, lby, rbxOn)
+    x, y = apply_reflection(x, y, crossOutAbove, crossOutBelow,  crossInAbove, crossInBelow, uby, lby, rbxOn)
 
     inside = (y<uby) & (y>lby) # Particles inside the fracture
     outsideAbove = y>uby # Particles in the porous matrix above the fracture
     outsideBelow = y<lby #Particles in the porous matrix below the fracture
 
-    pdf_part[int(t/dt)] = sum(x[isIn]>rbx) # Count the particle which exit the right boundary at each time step
+    pdf_part[int(t/dt)] = sum(abs(x[isIn])>rbx) # Count the particle which exit the right boundary at each time step
+
+    if t == recordSpatialConc:
+        countsSpace, binEdgeSpace = np.histogram(x, xBins, density=True)
 
     cdf = sum(pdf_part)
     t += dt    
@@ -137,7 +151,7 @@ if plotCharts and recordTrajectories:
         plt.plot(xPath[i], yPath[i], lw=0.5)
     plt.axhline(y=uby, color='r', linestyle='--', linewidth=2)
     plt.axhline(y=lby, color='r', linestyle='--', linewidth=2)
-    if yesRbx:
+    if rbxOn:
         plt.axvline(x=lbx, color='black', linestyle='-', linewidth=2)
     plt.title("2D Diffusion Process (Langevin Equation)")
     plt.xlabel("X Position")
@@ -168,6 +182,11 @@ plt.plot(binEdgesLog[1:][countsLog!=0], countsLog[countsLog!=0], 'r*')
 plt.xscale('log')
 plt.yscale('log')
 
+# Spatial concentration profile at 'recordSpatialConc' time
+plt.figure(figsize=(8, 8))
+plt.plot(binEdgeSpace[1:][countsSpace!=0], countsSpace[countsSpace!=0], 'b-')
+if rbxOn:
+    plt.axvline(x=lbx, color='black', linestyle='-', linewidth=2)
 
 # Statistichs
 print(f"Execution time: {execution_time:.6f} seconds")
