Normalisation and dt bug fixed

Python/diffAndReact2Dmatrix_vectorised.py
@@ -8,8 +8,8 @@ import time
 # Features ###################################################################
 plotCharts = True # It controls graphical features (disable when run on HPC)
 recordTrajectories = False # It uses up memory
-degradation = False # Switch for the degradation of the particles
-reflection = False # It defines the upper and lower fracture's walls behaviour, wheather particles are reflected or adsorpted
+degradation = True # Switch for the degradation of the particles
+reflection = True # It defines the upper and lower fracture's walls behaviour, wheather particles are reflected or adsorpted
 lbxOn = False # It controls the position of the left boundary
 lbxAdsorption = False # It controls whether the particles get adsorpted or reflected on the left boundary 
 stopOnCDF = False # Simulation is terminated when CDF reaches the stopBTC value
@@ -22,9 +22,9 @@ if plotCharts:
     from matplotlib.animation import FuncAnimation
 
 # Parameters #################################################################
-num_particles = int(1e6) # Number of particles in the simulation
-sim_time = int(500)
-dt = 1 # Time step
+num_particles = int(1e4) # Number of particles in the simulation
+sim_time = int(100)
+dt = 0.1 # Time step
 num_steps = int(sim_time/dt) # Number of steps
 Df = 0.1  # Diffusion for particles moving in the fracture
 Dm = 0.001  # Diffusion for particles moving in the porous matrix
@@ -259,8 +259,8 @@ iPart = 0
 iLbxOn = 0
 # Retrieve the number of steps for each particle from the pdf of the breakthrough curve
 for index, (valPdfPart, valLbxOn) in enumerate(zip(pdf_part, pdf_lbxOn)):
-    particleRT.extend([index*dt+1]*int(valPdfPart))
-    particleSemiInfRT.extend([index*dt+1]*int(valLbxOn))
+    particleRT.extend([index+1]*int(valPdfPart))
+    particleSemiInfRT.extend([index+1]*int(valLbxOn))
     iPart = iPart+valPdfPart
     iLbxOn = iLbxOn+valLbxOn
 
@@ -315,9 +315,9 @@ if recordTrajectories and np.logical_not(reflection):
 # Survival time distribution
 
 liveParticlesInTime = np.sum(particleSteps[:, None] > timeStep, axis=0)
-liveParticlesInTimeNorm = liveParticlesInTime/liveParticlesInTime.sum()
+liveParticlesInTimeNorm = liveParticlesInTime/sum(liveParticlesInTime*np.diff(np.insert(timeStep, 0, 0)))
 liveParticlesInLogTime = np.sum(particleSteps[:, None] > timeLogSpaced, axis=0)
-liveParticlesInLogTimeNorm = liveParticlesInLogTime/liveParticlesInLogTime.sum()
+liveParticlesInLogTimeNorm = liveParticlesInLogTime/sum(liveParticlesInLogTime*np.diff(np.insert(timeLogSpaced, 0, 0)))
 
 # Statistichs ########################################################################
 print(f"Execution time: {execution_time:.6f} seconds")
@@ -338,8 +338,8 @@ variablesToSave = {name: value for name, value in globals().items() if isinstanc
 # Save all the variables to an .npz file
 # np.savez('infiniteDomain1e6.npz', **variablesToSave)
 # np.savez('semiInfiniteDomain1e3.npz', **variablesToSave)
-# np.savez('degradation_3.npz', **variablesToSave)
-np.savez('totalAdsorption_3.npz', **variablesToSave)
+np.savez('degradation_4.npz', **variablesToSave)
+# np.savez('totalAdsorption_4.npz', **variablesToSave)
 # np.savez('finalPositions1e5.npz', **variablesToSave)
 # np.savez('testSemra.npz', **variablesToSave)
 # np.savez('matrixDiffusionVerification.npz', **variablesToSave)

Python/plotCharts.py
@@ -76,19 +76,19 @@ if plotTrajectories:
 if plotEulerianPdfCdf:
     # PDF
     plt.figure(figsize=(8, 8))
-    plt.plot(timeStep*dt, pdf_part/num_particles)
+    plt.plot(timeStep, pdf_part/num_particles)
     plt.xscale('log')
     plt.title("PDF")
 
     # CDF
     plt.figure(figsize=(8, 8))
-    plt.plot(timeStep*dt, np.cumsum(pdf_part)/num_particles)
+    plt.plot(timeStep, np.cumsum(pdf_part)/num_particles)
     plt.xscale('log')
     plt.title("CDF")
 
     # 1-CDF
     plt.figure(figsize=(8, 8))
-    plt.plot(timeStep*dt, 1-np.cumsum(pdf_part)/num_particles)
+    plt.plot(timeStep, 1-np.cumsum(pdf_part)/num_particles)
     plt.xscale('log')
     plt.yscale('log')
     plt.title("1-CDF")
@@ -142,8 +142,8 @@ if plotDegradation:
 
     # Distribution of live particles in time
     survivalTimeDistribution = plt.figure(figsize=(8, 8))
-    plt.plot(timeStep*dt, exp_prob, 'r-')
-    plt.plot(timeStep*dt, liveParticlesInTimeNormDeg, 'b*')
+    plt.plot(timeStep, exp_prob, 'r-')
+    plt.plot(timeStep, liveParticlesInTimeNormDeg, 'b*')
     plt.title("Live particle distribution in time")
     plt.xscale('log')
     plt.yscale('log')
@@ -200,7 +200,7 @@ if plotFinalPositions:
 if plotSruvivalTimeDistOfNonAdsorbed:
     # Normalised distribution of non-absorbed particles in time
     diffusionLimitedSurvTimeDistNorm = plt.figure(figsize=(8, 8))
-    plt.plot(timeStep*dt, liveParticlesInTimeNormAds, 'b*')
+    plt.plot(timeStep, liveParticlesInTimeNormAds, 'b*')
     plt.title("Non-absorbed particle normalised in time")
     plt.xscale('log')
     plt.yscale('log')
@@ -219,8 +219,8 @@ if plotSurvivalTimeDistAndReactionRatesForDegradationAndAdsorption:
     tau = (uby-lby)**2/Df
     # exp_decay = np.exp(-Time/tau)
     # plt.plot(Time[:-1], np.log(exp_decay[:-1]), label=f'p_s(t)=e^(-t/tau) where tau_d={tau}', color='r')
-    plt.plot(timeStep*dt/tau, liveParticlesInTimeNormDeg, label=r'$p_s(t)=ke^{-kt} \, lin \, bins$', color='blue')
-    plt.plot(timeStep*dt/tau, liveParticlesInTimeNormAds, label=r'$p_s(t)=ads \, bc \, lin \, bins$', color='green')
+    plt.plot(timeStep/tau, liveParticlesInTimeNormDeg, label=r'$p_s(t)=ke^{-kt} \, lin \, bins$', color='blue')
+    plt.plot(timeStep/tau, liveParticlesInTimeNormAds, label=r'$p_s(t)=ads \, bc \, lin \, bins$', color='green')
     plt.plot(timeLogSpaced/tau, liveParticlesInLogTimeNormDeg, label=r'$p_s(t)=ke^{-kt} \, log \, bins$', color='b', linestyle='--')
     plt.plot(timeLogSpaced/tau, liveParticlesInLogTimeNormAds, label=r'$p_s(t)=ads \, bc \, log \, bins$', color='g', linestyle='--')
     plt.title("Survival time PDFs")
@@ -237,12 +237,12 @@ if plotSurvivalTimeDistAndReactionRatesForDegradationAndAdsorption:
     # Rates of particles decay
     compareDecayDegradationRatesLin = plt.figure(figsize=(8, 8))
     plt.rcParams.update({'font.size': 20})
-    tDiff = np.diff(timeStep*dt)
+    tDiff = np.diff(timeStep)
     dSurvPart = np.diff(np.log(liveParticlesInTimeNormDeg))
     dNonAdsPart = np.diff(np.log(liveParticlesInTimeNormAds))
     dSurvdt = dSurvPart/tDiff
     dNonAdsdt = dNonAdsPart/tDiff
-    midTimes = ((timeStep*dt)[:-1] + (timeStep*dt)[1:]) / 2
+    midTimes = ((timeStep)[:-1] + (timeStep)[1:]) / 2
     plt.plot(midTimes/tau, dSurvdt, label='Well mixed lin bins', color='b') # , marker='+', linestyle='none', markersize='5')
     plt.plot(midTimes/tau, dNonAdsdt, label='Diff limited lin bins', color='g') # , marker='x', linestyle='none', markersize='5')
     plt.title("Effective reaction rate")
@@ -260,11 +260,12 @@ if plotSurvivalTimeDistAndReactionRatesForDegradationAndAdsorption:
     dSurvLogdt = dSurvPartLog/tLogDiff
     dNonAdsLogdt = dNonAdsPartLog/tLogDiff
     midTimesLog = (timeLogSpaced[:-1] + timeLogSpaced[1:]) / 2
-    plt.plot(midTimesLog/tau, dSurvLogdt, label='Well mixed log bins', color='b', linestyle='--', linewidth='5') # marker='p', linestyle='none', markersize='5')
+    plt.plot(midTimesLog/tau, dSurvLogdt, label='Well mixed log bins', color='b', linestyle='--') # marker='p', linestyle='none', markersize='5')
     plt.plot(midTimesLog/tau, dNonAdsLogdt, label='Diff limited log bins', color='g', linestyle='--') # marker='*', linestyle='none', markersize='5')
     plt.title("Effective reaction rate")
     plt.xlabel('Time/tau')
     plt.ylabel('k(t)')
+    plt.ylim(-0.7, 0.01)
     plt.xscale('log')
     plt.grid(True, which="major", linestyle='-', linewidth=0.7, color='gray')
     plt.legend(loc='best')