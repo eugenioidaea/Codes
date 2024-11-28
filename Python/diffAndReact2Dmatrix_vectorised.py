f4a6e50f54355b53187aaa2071693410249d3e96
eugenioidaea <eugenio.pescimoro@idaea.csic.es>
2024-11-28 10:57:12 +0100

    Start debugging Matrix 2D Verification

--- Python/diffAndReact2Dmatrix_vectorised.py
+++ Python/diffAndReact2Dmatrix_vectorised.py
@@ -9,12 +9,12 @@ import time
 plotCharts = True # It controls graphical features (disable when run on HPC)
 recordTrajectories = False # It uses up memory
 degradation = False # Switch for the degradation of the particles
-reflection = False # If False, the particles get adsorbed
+reflection = True # If False, the particles get adsorbed
 lbxOn = False # It controls the position of the left boundary
 lbxAdsorption = False # It controls whether the particles get adsorpted or reflected on the left boundary 
 stopOnCDF = False # Simulation is terminated when CDF reaches the stopBTC value
 vcpOn = False # It regulates the visualisation of the vertical control plane
-matrixDiffVerification = False # It activates the matrix-diffusion verification testcase
+matrixDiffVerification = True # It activates the matrix-diffusion verification testcase
 # recordVideo = False # It slows down the script
 
 if plotCharts:
@@ -22,17 +22,17 @@ if plotCharts:
     from matplotlib.animation import FuncAnimation
 
 # Parameters #################################################################
-num_particles = int(1e5) # Number of particles in the simulation
-sim_time = int(8e3)
-dt = 0.1 # Time step
+num_particles = int(1e4) # Number of particles in the simulation
+sim_time = int(1e4)
+dt = 1 # Time step
 num_steps = int(sim_time/dt) # Number of steps
 Df = 0.1 # Diffusion for particles moving in the fracture
 Dm = 0.001  # Diffusion for particles moving in the porous matrix
 Dl = 0.1 # Diffusion left side of the domain (matrixDiffVerification only)
 Dr = 0.1 # Diffusion right side of the domain (matrixDiffVerification only)
 xInit = 0 # Initial horizontal position of the particles
-uby = 3 # Upper Boundary
-lby = -3 # Lower Boundary
+uby = 1 # Upper Boundary
+lby = -1 # Lower Boundary
 vcp = 10 # Vertical Control Plane
 if lbxOn:
     lbx = 0 # Left Boundary X
@@ -391,62 +391,14 @@ variablesToSave = {name: value for name, value in globals().items() if isinstanc
 # np.savez('compareAp6.npz', **variablesToSave)
 
 # Final particles's positions
-# finalPositions = plt.figure(figsize=(8, 8))
-# if matrixDiffVerification:
-#     plt.plot(x, y, 'b*')
-#     plt.plot([lbx, rbx, rbx, lbx, lbx], [lby, lby, uby, uby, lby], color='black', linewidth=2)
-#     plt.scatter(x0, y0, s=2, c='purple', alpha=1, edgecolor='none', marker='o')
-#     if (reflectedLeft!=0) & (reflectedRight!=0):
-#         plt.plot([cbx, cbx], [lby, uby], color='orange', linewidth=3, linestyle='--')
-#     histoMatriDiff = plt.figure(figsize=(8, 8))
-#     hDist, hBins = np.histogram(x, np.linspace(lbx, rbx, 100), density=True)
-#     plt.bar(hBins[:-1], hDist, width=np.diff(hBins), edgecolor="black", align="edge")
-#     plt.axvline(x=cbx, color='orange', linestyle='-', linewidth=2)
-
-
-
-
-# liveParticlesInTimeD1 = liveParticlesInTime.copy()
-# liveParticlesInTimeNormD1 = liveParticlesInTimeNorm.copy()
-# tauD1 = (uby-lby)**2/Df
-# liveParticlesInTimeD01 = liveParticlesInTime.copy()
-# liveParticlesInTimeNormD01 = liveParticlesInTimeNorm.copy()
-# tauD01 = (uby-lby)**2/Df
-# liveParticlesInTimeD001 = liveParticlesInTime.copy()
-# liveParticlesInTimeNormD001 = liveParticlesInTimeNorm.copy()
-# tauD001 = (uby-lby)**2/Df
-
-
-
-
-# Distribution of live particles in time
-# survTimeDistCompareDiff = plt.figure(figsize=(8, 8))
-# plt.rcParams.update({'font.size': 20})
-# plt.plot(timeLinSpaced, liveParticlesInTimeD1, label=r'$D_f = 1$', color='b', linestyle='-')
-# plt.plot(timeLinSpaced, liveParticlesInTimeD01, label=r'$D_f = 0.1$', color='r', linestyle='-')
-# plt.plot(timeLinSpaced, liveParticlesInTimeD001, label=r'$D_f = 0.01$', color='g', linestyle='-')
-# plt.title("Survival times")
-# plt.xscale('log')
-# plt.yscale('log')
-# plt.xlabel('Time')
-# plt.ylabel('Number of live particles')
-# plt.grid(True, which="major", linestyle='-', linewidth=0.7, color='black')
-# plt.grid(True, which="minor", linestyle=':', linewidth=0.5, color='gray')
-# plt.legend(loc='b# est')
-# plt.tight_layout(# )
-
-# Normalised distribution of live particles in time
-# survTimeDistCompareDiffNorm = plt.figure(figsize=(8, 8))
-# plt.rcParams.update({'font.size': 20})
-# plt.plot(timeLinSpaced/tauD1, liveParticlesInTimeNormD1, label=r'$D_f = 1$', color='b', linestyle='-')
-# plt.plot(timeLinSpaced/tauD01, liveParticlesInTimeNormD01, label=r'$D_f = 0.1$', color='r', linestyle='-')
-# plt.plot(timeLinSpaced/tauD001, liveParticlesInTimeNormD001, label=r'$D_f = 0.01$', color='g', linestyle='-')
-# plt.title("Survival time PDFs")
-# plt.xscale('log')
-# plt.yscale('log')
-# plt.xlabel(r'$Time/tau_i$')
-# plt.ylabel('Normalised number of live particles')
-# plt.grid(True, which="major", linestyle='-', linewidth=0.7, color='black')
-# plt.grid(True, which="minor", linestyle=':', linewidth=0.5, color='gray')
-# plt.legend(loc='best')
-# plt.tight_layout()
+finalPositions = plt.figure(figsize=(8, 8))
+if matrixDiffVerification:
+    plt.plot(x, y, 'b*')
+    plt.plot([lbx, rbx, rbx, lbx, lbx], [lby, lby, uby, uby, lby], color='black', linewidth=2)
+    plt.scatter(x0, y0, s=2, c='purple', alpha=1, edgecolor='none', marker='o')
+    if (reflectedLeft!=0) & (reflectedRight!=0):
+        plt.plot([cbx, cbx], [lby, uby], color='orange', linewidth=3, linestyle='--')
+    histoMatriDiff = plt.figure(figsize=(8, 8))
+    hDist, hBins = np.histogram(x, np.linspace(lbx, rbx, 100), density=True)
+    plt.bar(hBins[:-1], hDist, width=np.diff(hBins), edgecolor="black", align="edge")
+    plt.axvline(x=cbx, color='orange', linestyle='-', linewidth=2)
