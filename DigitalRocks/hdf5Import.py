ba745013ed9d8ca2928312a154f84c91dd05d479
eugenioidaea <eugenio.pescimoro@idaea.csic.es>
2025-02-28 23:52:35 +0100

    Cropping needs to be fixed

--- DigitalRocks/hdf5Import.py
+++ DigitalRocks/hdf5Import.py
@@ -6,36 +6,36 @@ from skimage.transform import resize
 import openpnm as op
 import matplotlib.transforms as transforms
 
-ts = 0.70 # Threshold value: above is fracture (0), below is matrix (1)
-xl = 7000
-xr = 7100 # 10000
-yb = 1000
-yt = 1050 # 8000
+ts = 0.95 # Threshold value: above is fracture, below is matrix
+xl = 7000 # 1000
+xr = 9000# 1100 # 8000
+yb = 5500
+yt = 6500 # 7200
 
 imageTif = tiff.imread("/home/eugenio/Downloads/G4.tif") # For 2D images it is a simple matrix with values between 0 (white->solid matrix) and 255 (black->fracture and pores)
 imageTif = np.flipud(imageTif)
 imageComplete = resize(imageTif, (imageTif.shape[0], imageTif.shape[0]))
-image = imageTif[yb:yt, xl:xr]
-image = resize(image, (image.shape[0], image.shape[1])) # It scales the values between 0 (white) and 1 (black)
-binIm = np.zeros((yt-yb, xr-xl)) # It crops the image
-binIm[image>ts] = 0 # Binary transformation
-binIm[image<ts] = 1 # Binary transformation
+imageCrop = imageTif[yb:yt, xl:xr]
+imageCrop = resize(imageCrop, (imageCrop.shape[0], imageCrop.shape[1])) # It scales the values between 0 (white) and 1 (black)
+binIm = np.zeros((yt-yb, xr-xl)) # It crops the imageCrop
+binIm[imageCrop>ts] = 0 # Fracture
+binIm[imageCrop<ts] = 1 # Matrix
 
 imageFull = plt.figure(figsize=(8, 8))
 plt.imshow(imageComplete, cmap='gray', origin='lower')
 plt.plot([xl, xr, xr, xl, xl], [yb, yb, yt, yt, yb])
 grayScale = plt.figure(figsize=(8, 8))
-plt.imshow(image, cmap='gray')
+plt.imshow(imageCrop, cmap='gray', origin='lower')
 binary = plt.figure(figsize=(8, 8))
-plt.imshow(binIm, cmap='gray')
+plt.imshow(binIm, cmap='gray', origin='lower')
 
 net = ps.networks.snow2(binIm) #, voxel_size=1)
 pn = op.io.network_from_porespy(net.network)
 
 # Transpose the pore coordinates (swap X and Y)
-# pn["pore.coords"][:, [0, 1]] = pn["pore.coords"][:, [0, 1]]
+pn["pore.coords"][:, [1, 0]] = pn["pore.coords"][:, [0, 1]]
 poreNetwork = plt.figure(figsize=(8, 8))
-plt.imshow(binIm, cmap=plt.cm.bone)
+plt.imshow(np.flipud(binIm), cmap=plt.cm.bone)
 op.visualization.plot_coordinates(ax=poreNetwork,
                                   network=pn,
                                   size_by=pn["pore.inscribed_diameter"],
