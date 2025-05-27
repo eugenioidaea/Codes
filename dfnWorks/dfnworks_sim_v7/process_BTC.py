58e1da50dda065dcbf89faa049e8e3871ddca515
eugenioidaea <eugenio.pescimoro@idaea.csic.es>
2025-05-27 12:13:37 +0200

    Analytical solution needs to be fixed

--- dfnWorks/dfnworks_sim_v7/process_BTC.py
+++ dfnWorks/dfnworks_sim_v7/process_BTC.py
@@ -25,24 +25,27 @@ mas_filename = "output/dfn_diffusion_no_flow-mas.dat"
 
 df = load_mas_file(mas_filename)
 
-n = 50
-D = 1e-6
+Jsim = np.array(df['OUTFLOW TRACER [mol/y]'])
 C1 = 1
+Atot = 1e0
 L = 20
+Deff = -Jsim[-1]*L/(C1*Atot)
+
+n = 50
 time = np.array(df['Time [y]'])
 def CJsol(t, n):
     nArray = np.linspace(1, n, int(n))
     cos = np.cos(np.pi*nArray)
-    e1 = np.exp(-D*nArray**2*np.pi**2/L**2)
+    e1 = np.exp(-Deff*nArray**2*np.pi**2/L**2)
     e2 = np.exp(t/L**2)
-    J = D*C1/L+2*C1/L*np.sum(cos*e1)*e2
+    J = Deff*C1/L+2*C1/L*np.sum(cos*e1)*e2
     return J
 Jtot = CJsol(time, n)
 
 fig,ax = plt.subplots(figsize = (8,6))
 plt.rcParams.update({'font.size': 20})
 ax.plot(df['Time [y]'], -1*df['OUTFLOW TRACER [mol/y]'])
-ax.plot(time, -Jtot*1e4)
+ax.plot(time, Jtot*Atot)
 plt.title('Breakthrough curve')
 plt.xlabel('Time [y]')
 plt.ylabel('Outflowing tracer [mol/y]')
