ec0b67ec65ac623dfd4eb86bf5dc7a3763191a70
eugenioidaea <eugenio.pescimoro@idaea.csic.es>
2025-06-10 16:28:36 +0200

    Analytical bug fixed

--- dfnWorks/dfnworks_sim_v7/process_BTC.py
+++ dfnWorks/dfnworks_sim_v7/process_BTC.py
@@ -27,37 +27,58 @@ df = load_mas_file(mas_filename)
 
 Jsim = np.array(df['OUTFLOW TRACER [mol/y]'])
 C1 = 1
-Atot = 1e0
+Atot = 2 # 3.46410e-05*10.0
 L = 20
 Deff = -Jsim[-1]*L/(C1*Atot)
-
 n = 50
 time = np.array(df['Time [y]'])
-def CJsol(t, n):
-    nArray = np.linspace(1, n, int(n))
-    cos = np.cos(np.pi*nArray)
-    e1 = np.exp(-Deff*nArray**2*np.pi**2/L**2)
-    e2 = np.exp(t/L**2)
-    J = Deff*C1/L+2*C1/L*np.sum(cos*e1)*e2
-    return J
-Jtot = CJsol(time, n)
+x = 20.0  # Distance between inlet and btc record section
 
-fig,ax = plt.subplots(figsize = (8,6))
+def CJsolFor(t_array, n_terms, x):
+    n = np.arange(1, n_terms + 1)
+    t = t_array
+    J = np.zeros(len(time))
+
+    for ti in range(0, len(t)):
+        cos_part = np.cos(n * np.pi * x / L)
+        exp_part = np.exp(-Deff * (n**2) * np.pi**2 * t[ti] / L**2)
+        J[ti] = Deff*C1/L + (2*C1/L) * np.sum(cos_part*exp_part)
+
+    return J
+Jfor = CJsolFor(time, n, x)
+
+def CJsolVector(t_array, n_terms, x):
+    n = np.arange(1, n_terms + 1).reshape(-1, 1)  # shape (n_terms, 1)
+    t = t_array.reshape(1, -1)  # shape (1, num_times)
+    
+    cos_part = np.cos(n * np.pi * x / L)  # shape (n_terms, 1)
+    exp_part = np.exp(-Deff * (n**2) * np.pi**2 * t / L**2)  # shape (n_terms, num_times)
+    
+    sum_terms = cos_part * exp_part  # shape (n_terms, num_times)
+    sum_result = np.sum(sum_terms, axis=0)  # sum over n
+    
+    J = Deff*C1/L + (2*C1/L) * sum_result  # shape (num_times,)
+    return J
+Jvec = CJsolVector(time, n, x)
+
+fig, ax = plt.subplots(figsize = (8,6))
 plt.rcParams.update({'font.size': 20})
-ax.plot(df['Time [y]'], -1*df['OUTFLOW TRACER [mol/y]'])
-ax.plot(time, Jtot*Atot)
-plt.title('Breakthrough curve')
+ax.plot(df['Time [y]'], -1*df['OUTFLOW TRACER [mol/y]'], 'o', markerfacecolor='none', markeredgecolor='red', markersize='5', label='Numerical')
+# ax.plot(time, Jvec*Atot)
+ax.plot(time, Jfor*Atot, color='blue', linewidth=3, label='Analytical')
+# plt.title('Breakthrough curve')
 plt.xlabel('Time [y]')
 plt.ylabel('Outflowing tracer [mol/y]')
 plt.xscale('log')
 # plt.yscale('log')
+plt.legend(loc='best')
 plt.grid(True)
 plt.show()
 
 # cNorm = -1*df['OUTFLOW TRACER [mol/y]']/np.sum(-1*df['OUTFLOW TRACER [mol/y]'])
 cPlateau1 = np.array(-1*df['OUTFLOW TRACER [mol/y]']/np.max(-1*df['OUTFLOW TRACER [mol/y]']))
 time = np.array(df['Time [y]'])
-fig,ax = plt.subplots(figsize = (8,6))
+fig, ax = plt.subplots(figsize = (8,6))
 plt.rcParams.update({'font.size': 20})
 ax.plot(time, cPlateau1)
 plt.title('Plateau normalised BTC')
@@ -84,7 +105,7 @@ VarT = m2t - m1t**2
 Ddisp = m2t/(2*m1t)
 # Deff = Ddisp/advVel
 
-fig,ax = plt.subplots(figsize = (8,6))
+fig, ax = plt.subplots(figsize = (8,6))
 plt.rcParams.update({'font.size': 20})
 ax.plot(df['Time [y]'], 1-cPlateau1)
 plt.title('Complementary plateau norm BTC')
