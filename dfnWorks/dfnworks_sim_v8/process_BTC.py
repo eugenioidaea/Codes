7c8ae8197810a4ca4a4e3923802d0b1a29036521
eugenioidaea <eugenio.pescimoro@idaea.csic.es>
2025-06-25 18:49:47 +0200

    More central points thanks to specified time spacing -> better match

--- dfnWorks/dfnworks_sim_v8/process_BTC.py
+++ dfnWorks/dfnworks_sim_v8/process_BTC.py
@@ -35,17 +35,17 @@ def load_mas_file(filename):
 #     J = J/np.max(J)    
 #     return J
 
-def CJsolVector(t_array, n_terms, x):
+def CJsolVector(t_array, n_terms, x, D):
     nArr = np.arange(1, n_terms + 1).reshape(-1, 1)  # shape (n_terms, 1)
     t = t_array.reshape(1, -1)  # shape (1, num_times)
     
     cos_part = np.cos(nArr * np.pi * x / L)  # shape (n_terms, 1)
-    exp_part = np.exp(-Deff * (nArr**2) * np.pi**2 * t / L**2)  # shape (n_terms, num_times)
+    exp_part = np.exp(-D * (nArr**2) * np.pi**2 * t / L**2)  # shape (n_terms, num_times)
     
     sum_terms = cos_part * exp_part  # shape (n_terms, num_times)
     sum_result = np.sum(sum_terms, axis=0)  # sum over n
     
-    J = Deff*C1/L * (1 + 2*sum_result) * Atot # shape (num_times,)
+    J = D*C1/L * (1 + 2*sum_result) * Atot # shape (num_times,)
     J = J/np.max(J)
     return J
 
@@ -115,6 +115,7 @@ mas_filename = "output/dfn_diffusion_no_flow-mas.dat"
 df = load_mas_file(mas_filename)
 
 # Input paramenters ####################################################
+Dmol = 4.5e-7 # [m2/s] DIFFUSION_COEFFICIENT from PFLOTRAN input file
 time = np.array(df['Time [y]'])*86400*365 # [s]
 Jsim = -np.array(df['OUTFLOW TRACER [mol/y]'])/(86400*365) # [mol/s]
 C1 = 1e3 # [mol/m3]
@@ -128,7 +129,7 @@ Jsim = Jsim/np.max(Jsim)
 
 # Compute analytical solution ##########################################
 # Jfor = CJsolFor(time, n, x)
-Jvec = CJsolVector(time, n, x)
+Jvec = CJsolVector(time, n, x, Deff)
 
 # OPTIMISTAION ###############################################################################
 # Initial guess for Deff
@@ -177,10 +178,11 @@ plt.show()
 # Numerical vs analytical vs optimised ######################################################
 fig, ax = plt.subplots(figsize = (8,6))
 plt.rcParams.update({'font.size': 20})
-ax.plot(time, Jsim, 'o', markerfacecolor='none', markeredgecolor='red', markersize='5', label='Numerical')
-ax.plot(time, Jvec, color='blue', linewidth=3, label='Analytical')
-ax.plot(time, lsqJvec(lsqDeff), '*', markerfacecolor='none', markeredgecolor='green', markersize='5', label='lsq')
-ax.plot(time, fitJvec(time, curveFitDeff), '*', markerfacecolor='none', markeredgecolor='pink', markersize='5', label='fit')
+ax.plot(time, Jsim, 'o', markerfacecolor='none', markeredgecolor='red', markersize='8', label='Numerical')
+ax.plot(time, CJsolVector(time, n, x, Dmol), '-', color='orange', linewidth=3, markersize='8', label='Dmol')
+ax.plot(time, Jvec, '--', color='blue', linewidth=3, label='Deff')
+ax.plot(time, lsqJvec(lsqDeff), '-*', color='green', linewidth=3, markersize='8', label='lsq')
+# ax.plot(time, fitJvec(time, curveFitDeff), '*', markerfacecolor='none', markeredgecolor='pink', markersize='5', label='fit')
 plt.xlabel('Time [s]')
 plt.ylabel('Outflowing tracer [mol/s]')
 plt.xscale('log')
@@ -189,10 +191,12 @@ plt.legend(loc='best')
 plt.grid(True)
 plt.show()
 
-print(f"Jsim Deff: {Deff:.4e}")
-print(f"Initial guess Deff {initial_guess[0]:.4e}")
-print(f"LSQ Deff: {lsqDeff:.4e}")
-print(f"Curve fit Deff: {curveFitDeff:.4e}")
+print(f"Dmol [m2/s] = {Dmol:.4e}")
+print(f"Deff [m2/s] = Jsim[-1]*L/(C1*Atot) = {Deff:.4e}")
+print(f"Initial guess : D0 [m2/s] = {initial_guess[0]:.4e}")
+print(f"Deff_LSQ [m2/s] = {lsqDeff:.4e}")
+# print(f"Curve fit Deff [m2/s]: {curveFitDeff:.4e}")
+print(f"Tortuosity: Dmol/Deff_LSQ = {Dmol/lsqDeff:.4}")
 
 # # cNorm = -1*df['OUTFLOW TRACER [mol/y]']/np.sum(-1*df['OUTFLOW TRACER [mol/y]'])
 # cPlateau1 = np.array(-1*df['OUTFLOW TRACER [mol/y]']/np.max(-1*df['OUTFLOW TRACER [mol/y]']))
